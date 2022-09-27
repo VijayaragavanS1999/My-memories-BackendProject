db11e788f3a594675adc41aaaf131295d42a4237
Your Name <you@example.com>
2022-09-27 23:52:51 +0530

    added procfile

--- app.js
+++ app.js
@@ -48,6 +48,13 @@ app.post("/compose", function (req, res) {
   posts.push(post);
   res.redirect("/");
 });
-app.listen(3000, function () {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
